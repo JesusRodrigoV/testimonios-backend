192c220a3e35727ea939da7db08b3fae6227befb
Andres Menchaca Rivero <4mrvro@gmail.com>
2025-05-11 01:54:22 -0400

    Creación de route para coleccion

---
+++ src/routes/coleccion.route.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import type { RequestHandler } from 'express';
+import { ColeccionController } from '../controllers/coleccion.controller';
+import { authenticateToken } from '@app/middleware/authentication';
+
+const router = express.Router();
+
+router.use(authenticateToken); // autenticación requerida para todas las rutas
+
+router.get('/', ColeccionController.getAll as RequestHandler);
+router.get('/:id', ColeccionController.getById as RequestHandler);
+router.post('/', ColeccionController.create as RequestHandler);
+router.put('/:id', ColeccionController.update as RequestHandler);
+router.delete('/:id', ColeccionController.delete as RequestHandler);
+
+// rutas para manejar testimonios en colecciones
+router.post('/testimonios', ColeccionController.addTestimonio as RequestHandler);
+router.delete('/:id_coleccion/testimonios/:id_testimonio', ColeccionController.removeTestimonio as RequestHandler);
+router.get('/:id/testimonios', ColeccionController.getTestimonios as RequestHandler);
+
+export default router;
